75167501d7f868c6826a68634aa43f1a3ec38ebf
Dan Williams <djwillia@us.ibm.com>
2016-08-31 11:29:31 -0400

    argument to build virtio or ukvm (or clean)

--- mirage-make/script.bash
+++ mirage-make/script.bash
@@ -7,13 +7,23 @@
 # just run the entrypoint under the same uid as the volume.  Instead
 # we do this horrible chown, rechown.
 
+T=$1
+shift
+
+if [ "x$T" != "xukvm" ] && [ "x$T" != "xvirtio" ] && [ "x$T" != "xclean" ]; then
+    echo "Usage: mirage-make TARGET"
+    echo "           where TARGET is either 'virtio' or 'ukvm'"
+    echo "       mirage-make clean"
+    exit 1;
+fi
+
 OLD_UG=`stat -c %u:%g /home/opam/src`;
 sudo chown -R opam:opam /home/opam/src;
 eval `opam config env`;
-if [[ $@ == "clean" ]]; then
+if [[ $T == "clean" ]]; then
     (cd /home/opam/src && make clean && mirage clean);
 else
-    opam pin add -y solo5-kernel-ukvm /home/opam/solo5;
-    (cd /home/opam/src && mirage configure -t ukvm && make);
+    opam pin add -y solo5-kernel-$T /home/opam/solo5;
+    (cd /home/opam/src && mirage configure -t $T && make);
 fi
 sudo chown -R $OLD_UG /home/opam/src;
