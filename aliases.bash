38961e9882eeb5df2e0e30011cfb26e891b21f4e
Dan Williams <djwillia@us.ibm.com>
2016-08-31 11:30:43 -0400

    add runner to aliases and build

--- aliases.bash
+++ aliases.bash
@@ -1,6 +1,21 @@
 
+# solo5-make [clean]
+#     runs make in the solo5 directory
 
-#alias solo5-make="docker run --rm -v $PWD:/src -u `id -u` solo5-make"
-alias solo5-make="docker run --rm -v $PWD:/src -u `id -u` solo5-make"
-#alias mirage-make="docker run --rm -v $PWD:/home/opam/src -v /home/djwillia/solo5:/home/opam/solo5 mirage-make"
-alias mirage-make="docker run --rm -v $PWD:/home/opam/src -v $PWD/../../solo5:/home/opam/solo5 mirage-make"
+alias solo5-make='docker run --rm -v $PWD:/src -u `id -u` solo5-make'
+
+# mirage-make TARGET [clean]
+#     where TARGET is either 'virtio' or 'ukvm'
+
+alias mirage-make='docker run --rm -v $PWD:/home/opam/src -v $PWD/../../solo5:/home/opam/solo5 mirage-make'
+
+# ukvm [--net] [--disk=foo] UNIKERNEL -- [unikernel args]
+# qemu [--net] [--disk=foo] UNIKERNEL -- [unikernel args]
+# kvm  [--net] [--disk=foo] UNIKERNEL -- [unikernel args]
+
+alias ukvm='docker run --rm --device=/dev/kvm:/dev/kvm --device=/dev/net/tun:/dev/net/tun --cap-add=NET_ADMIN -v $PWD:/src runner /src/ukvm-bin'
+alias qemu='docker run --rm --device=/dev/kvm:/dev/kvm --device=/dev/net/tun:/dev/net/tun --cap-add=NET_ADMIN -v $PWD:/src runner qemu'
+alias kvm='docker run --rm --device=/dev/kvm:/dev/kvm --device=/dev/net/tun:/dev/net/tun --cap-add=NET_ADMIN -v $PWD:/src runner kvm'
+
+
+
--- build.bash
+++ build.bash
@@ -5,7 +5,15 @@ mirage-make \
 solo5-make \
 objdump \
 "
+# runner  (built differently below)
 
 for f in $CONTAINERS; do
     docker build -f $f/Dockerfile -t $f $f
 done
+
+if [ ! -e docker_unikernel_runner ]; then
+    git clone -b combo https://github.com/djwillia/docker-unikernel-runner.git
+fi
+
+docker build -f docker-unikernel-runner/Dockerfile.runner-combo -t runner docker-unikernel-runner/
+
